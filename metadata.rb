daff731229378ffb2f5169c0fb41a140386b3221
Determe, Sebastien (sd378r) <sd378r@intl.att.com>
2016-10-07 06:55:04 -0700

    Meta-data requires by Berks

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-repo'
+maintainer       'at&t'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'chef-repo'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
